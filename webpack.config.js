8abead097c569b510345a2d54d31c692048c146a
Elena Stagg <elenagracestagg@hotmail.co.uk>
2020-05-14 15:19:32 +0100

    set up package.json, webpack config, hello world

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
